efc38f00d363439d58586d0256f4877e9280c30e
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-14 12:54:34 -0600

    Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+
 }
