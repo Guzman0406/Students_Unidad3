42e56999329f9b9889ced481136adec065534e82
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-14 12:38:05 -0600

    Clase de estudiante

--- app/src/main/java/com/example/student/data/Estudiante.kt
+++ app/src/main/java/com/example/student/data/Estudiante.kt
@@ -1,2 +1,10 @@
 package com.example.student.data
 
+data class Estudiante(
+    val id: Int = 0,
+    val nombre: String = "",
+    val apellido: String = "",
+    val grado: Int = 0,
+    val grupo: String = "",
+    val puntuaje: Double = 0.0
+)
